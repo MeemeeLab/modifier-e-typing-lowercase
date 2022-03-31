3f448504f51ca42ced3207c223b32fa97d0d0d98
MeemeeLab <57629983+MeemeeLab@users.noreply.github.com>
2022-04-01 06:11:40 +0900

    Add header for ETypingScriptModifier

--- scripts/1.ETypingScriptModifier.js
+++ scripts/1.ETypingScriptModifier.js
@@ -4,6 +4,12 @@ export default {
     match: /\/app\/jsa_std\/js\/app\.min\.js\?.*/g,
     response: (req, res, requestProxy) => {
         if (req.method === 'GET') {
+            res.writeHead(200, {
+                'Content-Type': 'application/javascript',
+                'Content-Length': fs.statSync('./../data/app.min.js').size,
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+                'Pragma': 'no-cache',
+            });
             fs.createReadStream('./../data/app.min.js').pipe(res);
         } else requestProxy(true);
     }
